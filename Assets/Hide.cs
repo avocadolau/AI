b33eed808389344d674bd9602eb75f5aa15f463e
Lau <64155460+avocadolau@users.noreply.github.com>
2022-09-29 09:42:32 +0200

    sdfa

---
+++ Assets/Hide.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class Hide : MonoBehaviour
+{
+    public float distance = 1.0f;
+    public GameObject target;
+    public NavMeshAgent agent;
+    public GameObject[] hidingSpots;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hidingSpots = GameObject.FindGameObjectsWithTag("HIDE");
+        HideAndRun();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    
+
+    void HideAndRun()
+    {
+        Func<GameObject, float> distance =
+            (hs) => Vector3.Distance(target.transform.position,
+                                     hs.transform.position);
+        GameObject hidingSpot = hidingSpots.Select(
+            ho => (distance(ho), ho)
+            ).Min().Item2;
+        Vector3 dir = hidingSpot.transform.position - target.transform.position;
+        Ray backRay = new Ray(hidingSpot.transform.position, -dir.normalized);
+        RaycastHit info;
+        hidingSpot.GetComponent<Collider>().Raycast(backRay, out info, 50f);
+        Debug.Log(info.point);
+
+        agent.destination = (info.point + dir.normalized);
+
+    }
+}
---
+++ Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class test : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+
+
+    private void Update()
+    {
+        Seek();
+    }
+
+    void Seek()
+    {
+        agent.destination = target.transform.position;
+    }
+}
